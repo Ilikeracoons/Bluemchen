d21f0cafa7456d7aa84a584db71f57e1751f06ab
Ilikeracoons <153775387+Ilikeracoons@users.noreply.github.com>
2024-01-05 06:37:27 +0100

    Added an uptime cmd

---
+++ Commands/Information/uptime.js
@@ -0,0 +1,13 @@
+module.exports = {
+/** @param {import(discord.js).message}message*/
+run: function run(message){
+    let totalSeconds = (message.client.uptime / 1000);
+    let days = Math.floor(totalSeconds / 86400);
+    totalSeconds %= 86400;
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = Math.floor(totalSeconds % 60);
+    return message.reply(`Ich bin online seit: ${days} Tagen, ${hours} Stunden, ${minutes} Minuten und ${seconds} Sekunden!  `)
+}
+};
