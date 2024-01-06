434296a942ffa1c4d8a88ab041fb1e3e44c8e0cb
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-01-06 15:48:58 +0100

    right click -> format document

--- Commands/Information/bye.js
+++ Commands/Information/bye.js
@@ -1,6 +1,6 @@
 module.exports = {
   /**@param {import('discord.js').Message}message*/
   run: function run(message) {
-      return message.reply(`Goodbye ${message.author}! See you soon :D`);
+    return message.reply(`Goodbye ${message.author}! See you soon :D`);
   }
 };
