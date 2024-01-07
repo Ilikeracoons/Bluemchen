404e2716ab353fd5cb9cc1eb4e4e7f5f57b7c0ef
Ilikeracoons <153775387+Ilikeracoons@users.noreply.github.com>
2024-01-07 18:39:03 +0100

    Changed language

--- Commands/Information/uptime.js
+++ Commands/Information/uptime.js
@@ -1,13 +1,21 @@
 module.exports = {
-  /** @param {import(discord.js).message}message*/
+  /**@param {import(discord.js).Message}message*/
   run: function run(message) {
     let totalSeconds = (message.client.uptime / 1000);
-    let days = Math.floor(totalSeconds / 86400);
+    const days = Math.floor(totalSeconds / 86400);
     totalSeconds %= 86400;
-    let hours = Math.floor(totalSeconds / 3600);
+    const hours = Math.floor(totalSeconds / 3600);
     totalSeconds %= 3600;
-    let minutes = Math.floor(totalSeconds / 60);
-    let seconds = Math.floor(totalSeconds % 60);
-    return message.reply(`Ich bin online seit: ${days} Tagen, ${hours} Stunden, ${minutes} Minuten und ${seconds} Sekunden!  `);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+
+
+    let msg = 'I am online since: ';
+    if (days) msg += `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+    else if (hours) msg += `${hours} hours, ${minutes} minutes and ${seconds} seconds!`;
+    else if (minutes) msg += `${minutes} minutes and ${seconds} seconds!`;
+    else msg += `${seconds} seconds!`;
+
+    return message.reply(msg);
   }
 };
