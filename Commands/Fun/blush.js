7fc7a6f1a2fdd800b4cecd9734387b55a97aae57
Ilikeracoons <153775387+Ilikeracoons@users.noreply.github.com>
2024-01-09 06:44:22 +0100

    Added blush

---
+++ Commands/Fun/blush.js
@@ -0,0 +1,15 @@
+const { EmbedBuilder } = require('discord.js');
+
+  const gifs = ['https://media.giphy.com/media/VVh7txo37uooM/giphy.gif','https://media.giphy.com/media/pPkXCRcslKzLO/giphy.gif','https://media.giphy.com/media/hOrsk8I7TWRXy/giphy.gif','https://media.giphy.com/media/ODECD7W3dzk5y/giphy.gif','https://media.giphy.com/media/BkqSYWqv8Zfva/giphy.gif','https://media.giphy.com/media/VpcYdQpElriNy/giphy.gif','https://media.giphy.com/media/KhhXPUPHlzfBS/giphy.gif','https://media.giphy.com/media/klmpEcFgXzrYQ/giphy.gif','https://media.giphy.com/media/T3Vvyi6SHJtXW/giphy.gif','https://media.giphy.com/media/36vZ9fy5nK0HS/giphy.gif','https://media.giphy.com/media/3IQsGPmOK8s4U/giphy.gif','https://media.giphy.com/media/cmB0ts2FBhPx9q3ZBY/giphy.gif','https://media.giphy.com/media/Q7MImfRX28tJ5uNkDX/giphy.gif','https://media.giphy.com/media/QzBXeaTZtg99PH9z6S/giphy.gif', 'https://media.giphy.com/media/VpcYdQpElriNy/giphy.gif',`https://media.giphy.com/media/dkvGrfQ6ryIAU/giphy.gif`,];
+
+module.exports = {
+ /**@param {import('discord.js').Message}message @param {string[]}args*/
+ run: async function run(message, args) {
+ await message.channel.send(`awww ${message.author} is bushing! `)
+ const randomIndex = Math.floor(Math.random() * gifs.length);
+ let embed = new EmbedBuilder().setImage(gifs[randomIndex]);
+ message.channel.send({ embeds: [embed] });
+ }
+};
+
+
