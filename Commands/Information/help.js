4e2dd91cfaa7ebd5376fc37b5936e12c31d99564
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-01-13 12:30:36 +0100

    added help command

---
+++ Commands/Information/help.js
@@ -0,0 +1,30 @@
+const { EmbedBuilder, Colors } = require('discord.js');
+
+module.exports = {
+  description: 'Get some help',
+
+  /**@param {import('discord.js').Message}message*/
+  run: function run(message) {
+    const
+      commands = message.client.commands.reduce((acc, v) => {
+        if (acc[v.category]) acc[v.category].push(v);
+        else acc[v.category] = [v];
+
+        return acc;
+      }, {}),
+      embeds = [
+        new EmbedBuilder({
+          title: 'My commands',
+          color: Colors.White,
+          thumbnail: { url: message.client.user.avatarURL() }
+        }),
+        ...Object.values(commands).map(categoryCommands => new EmbedBuilder({
+          title: categoryCommands[0].category,
+          color: Colors.White,
+          fields: categoryCommands.map(e => ({ name: e.name, value: e.description || 'No description available', inline: true }))
+        }))
+      ];
+
+    return message.reply({ embeds });
+  }
+};
