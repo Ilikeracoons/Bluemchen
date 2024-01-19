7722eb81a73d14c3d0f8453097102b4e05c49724
Ilikeracoons <153775387+Ilikeracoons@users.noreply.github.com>
2024-01-19 22:46:24 +0100

    hiding owner-only for non owner

--- Commands/Information/help.js
+++ Commands/Information/help.js
@@ -1,4 +1,6 @@
-const { EmbedBuilder, Colors } = require('discord.js');
+const
+  { EmbedBuilder, Colors } = require('discord.js'),
+  config = require('../../config.json');
 
 module.exports = {
   description: 'Get some help',
@@ -6,21 +8,25 @@ module.exports = {
   /**@param {import('discord.js').Message}message*/
   run: function run(message) {
     const
-      commands = message.client.commands.reduce((acc, v) => {
-        if (acc[v.category]) acc[v.category].push(v);
-        else acc[v.category] = [v];
-
+      commands = message.client.commands.reduce((acc, command) => {
+        if (
+          !(config?.ownerOnlyFolders ?? ['owner-only']).includes(command.category.toLowerCase())
+          || (config?.ownerIds ?? [message.client.application.owner.id]).includes(message.author.id)
+        ) {
+          if (acc[command.category]) acc[command.category].push(command);
+          else acc[command.category] = [command];
+        }
         return acc;
       }, {}),
       embeds = [
         new EmbedBuilder({
           title: 'Look at my commands! ^^',
-          color: Colors.White,
+          color: Colors.DarkGreen,
           thumbnail: { url: message.client.user.avatarURL() }
         }),
         ...Object.values(commands).map(categoryCommands => new EmbedBuilder({
           title: categoryCommands[0].category,
-          color: Colors.White,
+          color: Colors.DarkGreen,
           fields: categoryCommands.map(e => ({ name: e.name, value: e.description || 'No description available', inline: true }))
         }))
       ];
