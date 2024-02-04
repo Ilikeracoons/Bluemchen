b0cd5048883f726b16925935b4319f9b5c0a030d
Ilikeracoons <153775387+Ilikeracoons@users.noreply.github.com>
2024-02-04 18:08:53 +0100

    deleted hello and bye

--- Commands/Fun/bye.js
+++
@@ -1,8 +0,0 @@
-/**@type {command}*/
-module.exports = {
-  description: 'tells User goodbye',
-  
-  run: function run(message) {
-    return message.reply(`Goodbye ${message.author}! See you soon :D`);
-  }
-};
--- Commands/Fun/hello.js
+++
@@ -1,8 +0,0 @@
-/**@type {command}*/
-module.exports = {
-  description: 'Says Hello',
-  
-  run: function run(message) {
-    return message.reply('Hello! ^^');
-  }
-};
