d7abd1b3585daa6e17f20cf8515923161a1dbcb8
Ilikeracoons <153775387+Ilikeracoons@users.noreply.github.com>
2024-02-04 16:18:49 +0100

    Added a new gif to bully people

--- Commands/Fun/bully.js
+++ Commands/Fun/bully.js
@@ -1,7 +1,8 @@
 const { Colors, EmbedBuilder } = require('discord.js');
 const gifs = [
   'https://media1.tenor.com/m/xy05so3mD6cAAAAC/please-dont-bully-me-nagatoro-san-anime.gif', 'https://media1.tenor.com/m/fdvjUM36k8oAAAAd/super_mario_3d_world-bully.gif',
-  'https://media1.tenor.com/m/77NmM3gal6sAAAAC/kitten-cute.gif', 'https://media1.tenor.com/m/Yv3P2LUfeLsAAAAC/nelson-simpsons.gif'
+  'https://media1.tenor.com/m/77NmM3gal6sAAAAC/kitten-cute.gif', 'https://media1.tenor.com/m/Yv3P2LUfeLsAAAAC/nelson-simpsons.gif',
+  'https://media1.tenor.com/m/PBdwTKAond0AAAAC/pokemon-will.gif'
 ];
 
 /**@type {command}*/
