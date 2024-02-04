12d3327517c8c600c2cfeb724b4727763b7cec10
Ilikeracoons <153775387+Ilikeracoons@users.noreply.github.com>
2024-02-04 18:08:03 +0100

    added color cmd

---
+++ Commands/Fun/kiss.js
@@ -0,0 +1,25 @@
+const { Colors, EmbedBuilder } = require('discord.js');
+const gifs = ['https://media1.tenor.com/m/Ko1AOLzUmyEAAAAd/kiss-anime.gif', 'https://media1.tenor.com/m/OjcDtiEDUvMAAAAC/friendly-kiss.gif',
+  'https://media1.tenor.com/m/rQ8qlj_oQ-YAAAAC/anime-kiss.gif', 'https://media1.tenor.com/m/xb1sLPwwRScAAAAd/akagi-miria-miria-akagi.gif',
+  'https://media1.tenor.com/m/H7ElWf1bKUkAAAAC/anime-kiss-miyamura-kiss.gif', 'https://media1.tenor.com/m/EaU6_8vbbuQAAAAC/love-liebe.gif',
+  'https://media1.tenor.com/m/_eP0iWZ2Y5AAAAAC/chibi-cat-mochi-cat.gif', 'https://media1.tenor.com/m/BZyWzw2d5tAAAAAC/hyakkano-100-girlfriends.gif',
+  'https://media1.tenor.com/m/PS6medrGxqwAAAAd/cat-kiss.gif', 'https://media1.tenor.com/m/ye6xtORyw_8AAAAC/2.gif',
+  'https://media1.tenor.com/m/SZ8-4vDwi6cAAAAC/miyamura-hori.gif', 'https://media1.tenor.com/m/PzTWDtTn_N4AAAAC/kiss-blow-kisses.gif',
+  'https://media1.tenor.com/m/WaJl99zHUYIAAAAC/love.gif', 'https://media1.tenor.com/m/1wObtysLtHEAAAAC/aaa-bbb.gif',
+  'https://media1.tenor.com/m/Q6rgr_3z9W0AAAAC/kiss.gif',
+];
+
+/**@type {command}*/
+module.exports = {
+  description: 'Gives a user a kiss',
+
+
+  run: async function run(message) {
+    const firstUserMentioned = message.mentions.users.first();
+    if (firstUserMentioned);
+    const userMentioned = firstUserMentioned?.username;
+    const randomIndex = Math.floor(Math.random() * gifs.length);
+    const embed = new EmbedBuilder().setDescription(`Mwuahhh ${userMentioned || message.author}`).setImage(gifs[randomIndex]).setColor(Colors.DarkGreen);
+    return message.reply({ embeds: [embed] });
+  }
+};
