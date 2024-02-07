332b51fbecfe898bb468f431befc513546598967
Ilikeracoons <153775387+Ilikeracoons@users.noreply.github.com>
2024-02-07 19:23:03 +0100

    Added embed to userinfo

--- Commands/Information/userinfo.js
+++ Commands/Information/userinfo.js
@@ -1,15 +1,15 @@
+const { Colors, EmbedBuilder } = require('discord.js');
 /**@type {command}*/
 module.exports = {
   description: 'Get information about yourself from me :D',
-  
+
   run: async function run(message) {
     const userMention = message.mentions.users.first() || message.author;
-
-    message.channel.send('User Information:\n\n' +
-      `Username: ${userMention.username}\n` +
+    const embed = new EmbedBuilder().setDescription(`Username: ${userMention.username}\n` +
       `Tag: ${userMention.tag}\n` +
       `Is Bot: ${userMention.bot}\n` +
       `Account Created At: ${userMention.createdAt}\n` +
-      `Avatar URL: ${userMention.avatarURL()}`);
+      `Avatar URL: ${userMention.avatarURL()}`).setColor(Colors.DarkGreen).setImage(userMention.avatarURL());
+    return message.reply({ embeds: [embed] });
   }
 };
