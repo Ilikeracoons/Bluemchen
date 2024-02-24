377b8cd8cdc8b0a8716c412fde1b258f2aec634d
Ilikeracoons <153775387+Ilikeracoons@users.noreply.github.com>
2024-02-24 18:59:23 +0100

    Added time events template

---
+++ Templates/timeevents.js
@@ -0,0 +1,13 @@
+module.exports = {
+  time: '',
+  startNow: false,
+  timeZone: undefined,
+
+  /** @this {Client}*/
+  onTick: async function () {
+
+  },
+  /** @this {Client}*/
+  onComplete: async function () {
+  },
+};
