1bf1f57b22d9405393242fd76000e0e5fe262ea6
Ilikeracoons <153775387+Ilikeracoons@users.noreply.github.com>
2024-02-28 19:18:07 +0100

    added an embed to restart

--- Commands/Owner-Only/restart.js
+++ Commands/Owner-Only/restart.js
@@ -1,9 +1,12 @@
+const { Colors, EmbedBuilder } = require('discord.js');
+
 /** @type {command}*/
 module.exports = {
   description: 'restart mee ^^',
 
-  run: async function run(message) {
-    await message.reply('I will restart now! gimmie a min');
-    process.exit();
+  run: function run(message) {
+    const embed = new EmbedBuilder().setDescription('Gonna restart now wait a min :t_rex:')
+      .setColor(Colors.DarkGreen);
+    return message.reply({ embeds: [embed] });
   }
 };
