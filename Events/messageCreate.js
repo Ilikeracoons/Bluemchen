16c47e972a4c35e294ea44d392b619f510001576
Ilikeracoons <153775387+Ilikeracoons@users.noreply.github.com>
2024-03-25 17:22:05 +0100

    Bluemchen will now react even if there is space between prefix and command name

--- Events/messageCreate.js
+++ Events/messageCreate.js
@@ -5,11 +5,10 @@ const
 /** @param {import('discord.js').Message}message*/
 module.exports = async function messageCreate(message) {
   // If the message author is a bot or the message doesn't start with the prefix, return.
-  if (message.author.bot || !message.content.toLowerCase().startsWith(prefix)) return;
-
+  if (message.author.bot || !message.content.startsWith(prefix + ' ')) return;
   const
     // remove the prefix from the string and then split the string on whitespaces.
-    args = message.content?.slice(prefix.length).split(' '),
+    args = message.content?.slice(prefix.length).trim().split(' '),
     // remove the first arg from the args array and save it to commandName
     commandName = args.shift();
 
