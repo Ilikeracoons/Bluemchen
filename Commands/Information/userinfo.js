4ecba1fb1c8063bf957a5d4575ad97870d044564
Ilikeracoons <153775387+Ilikeracoons@users.noreply.github.com>
2024-05-31 22:01:03 +0200

    Updated userinfo

--- Commands/Information/userinfo.js
+++ Commands/Information/userinfo.js
@@ -1,17 +1,45 @@
-const { Colors, EmbedBuilder } = require('discord.js');
+const { EmbedBuilder, Colors, ActivityType } = require('discord.js');
 
 /** @type {command}*/
 module.exports = {
   description: 'Get information about yourself from me :D',
 
-  run: function run(message) {
-    const userMention = message.mentions.users.first() || message.author;
-    const embed = new EmbedBuilder().setDescription(`Username: ${userMention.username}\n`
-      + `Tag: ${userMention.tag}\n`
-      + `Is Bot: ${userMention.bot}\n`
-      + `Account Created At: ${userMention.createdAt}\n`
-      + `Avatar URL: ${userMention.avatarURL()}`).setColor(Colors.DarkGreen)
-      .setImage(userMention.avatarURL());
-    return message.reply({ embeds: [embed] });
+  run: async function run(message) {
+    const member = message.mentions.members.first() || message.member;
+
+    const embed = new EmbedBuilder()
+      .setColor(Colors.DarkGreen)
+      .setTitle(`Some information about ${member.displayName}`)
+      .setThumbnail(member.user.avatarURL())
+      .addFields({ name: 'Username', value: `<@${member.id}>`, inline: true },
+
+        { name: 'Status', value: member.presence?.status || 'Offline', inline: true },
+
+        { name: 'Joined Discord on', value: `<t:${Math.round(member.user.createdTimestamp / 1000)}>`, inline: true },
+
+        { name: 'Joined this great sever on', value: `<t:${Math.round(member.joinedTimestamp / 1000)}>`, inline: true });
+
+    if (member.presence?.activities.length > 0) {
+      let activityString = '';
+      for (const activity of member.presence.activities) {
+        switch (activity.type) {
+          case ActivityType.Playing:
+            activityString += `Is playing **${activity.name}**`;
+            break;
+          case ActivityType.Listening:
+            activityString += `Is listening to **${activity.name}**`;
+            break;
+          case ActivityType.Watching:
+            activityString += `Is watching**${activity.name}**(porn)`;
+            break;
+          case ActivityType.Streaming:
+            activityString += `is streaming **${activity.name}**`;
+        }
+      }
+
+      embed.setDescription(activityString);
+    }
+
+    await message.reply({ embeds: [embed] });
   }
 };
