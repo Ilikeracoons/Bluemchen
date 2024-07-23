ab445c0939ffec3da9db0cf5c6a865a29b73ef39
Mephisto5558 <87113906+Mephisto5558@users.noreply.github.com>
2024-07-23 13:29:32 +0200

    replaced long giphy links with short ones using the Regular Expression `https://media.giphy.com/media/(.*?)/giphy.gif` -> `https://i.giphy.com/$1.gif`

--- Commands/Fun/blush.js
+++ Commands/Fun/blush.js
@@ -1,11 +1,11 @@
 const gifFunction = require('../../Utils/gifFunction.js');
 const gifs = [
-  'https://media.giphy.com/media/VVh7txo37uooM/giphy.gif', 'https://media.giphy.com/media/pPkXCRcslKzLO/giphy.gif', 'https://media.giphy.com/media/hOrsk8I7TWRXy/giphy.gif',
-  'https://media.giphy.com/media/ODECD7W3dzk5y/giphy.gif', 'https://media.giphy.com/media/BkqSYWqv8Zfva/giphy.gif', 'https://media.giphy.com/media/VpcYdQpElriNy/giphy.gif',
-  'https://media.giphy.com/media/KhhXPUPHlzfBS/giphy.gif', 'https://media.giphy.com/media/klmpEcFgXzrYQ/giphy.gif', 'https://media.giphy.com/media/T3Vvyi6SHJtXW/giphy.gif',
-  'https://media.giphy.com/media/36vZ9fy5nK0HS/giphy.gif', 'https://media.giphy.com/media/3IQsGPmOK8s4U/giphy.gif', 'https://media.giphy.com/media/cmB0ts2FBhPx9q3ZBY/giphy.gif',
-  'https://media.giphy.com/media/Q7MImfRX28tJ5uNkDX/giphy.gif', 'https://media.giphy.com/media/QzBXeaTZtg99PH9z6S/giphy.gif', 'https://media.giphy.com/media/VpcYdQpElriNy/giphy.gif',
-  'https://media.giphy.com/media/dkvGrfQ6ryIAU/giphy.gif'
+  'https://i.giphy.com/VVh7txo37uooM.gif', 'https://i.giphy.com/pPkXCRcslKzLO.gif', 'https://i.giphy.com/hOrsk8I7TWRXy.gif',
+  'https://i.giphy.com/ODECD7W3dzk5y.gif', 'https://i.giphy.com/BkqSYWqv8Zfva.gif', 'https://i.giphy.com/VpcYdQpElriNy.gif',
+  'https://i.giphy.com/KhhXPUPHlzfBS.gif', 'https://i.giphy.com/klmpEcFgXzrYQ.gif', 'https://i.giphy.com/T3Vvyi6SHJtXW.gif',
+  'https://i.giphy.com/36vZ9fy5nK0HS.gif', 'https://i.giphy.com/3IQsGPmOK8s4U.gif', 'https://i.giphy.com/cmB0ts2FBhPx9q3ZBY.gif',
+  'https://i.giphy.com/Q7MImfRX28tJ5uNkDX.gif', 'https://i.giphy.com/QzBXeaTZtg99PH9z6S.gif', 'https://i.giphy.com/VpcYdQpElriNy.gif',
+  'https://i.giphy.com/dkvGrfQ6ryIAU.gif'
 ];
 
 /** @type {command}*/
