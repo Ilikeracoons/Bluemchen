c4d62678b59e830df2b4e7a769fffba503440f8f
Ilikeracoons <153775387+Ilikeracoons@users.noreply.github.com>
2024-08-08 14:39:17 +0200

    added something

--- Commands/Information/userinfo.js
+++ Commands/Information/userinfo.js
@@ -7,6 +7,24 @@ module.exports = {
   run: async function run(message) {
     const member = message.mentions.members.first() || message.member;
 
+    if (member.id === message.client.user.id) {
+      const botEmbed = new EmbedBuilder()
+        .setColor(Colors.DarkGreen)
+        .setTitle(`Some Information about ${member.displayName} wait t-that is me! Okay here we go!`)
+        .setThumbnail(message.client.user.avatarURL())
+        .addFields(
+          {
+            name: 'my mom and sort of dad', value: 'Okay soo~ most important my Mothers name or just take this here [I_likeracoons](https://discord.com/users/1161364914011648074)\n'
+            + 'and her friend [Mephisto5558](https://discord.com/users/691550551825055775)'
+          },
+          { name: ' I am online ', value: message.client.user.presence?.status },
+          { name: 'I was born on the', value: `<t:${Math.round(member.user.createdTimestamp / 1000)}>`, inline: true },
+          { name: 'I joined this cute server on', value: `<t:${Math.round(member.joinedTimestamp / 1000)}>`, inline: true }
+        );
+      await message.reply({ embeds: [botEmbed] });
+      return;
+    }
+
     const embed = new EmbedBuilder()
       .setColor(Colors.DarkGreen)
       .setTitle(`Some information about ${member.displayName}`)
